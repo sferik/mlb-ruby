97585b4121dadd4bb93aa31462ff69171ac53878
Erik Michaels-Ober <sferik@gmail.com>
2012-10-09 23:26:35 -0700

    Replace should and should_not with expect

--- spec/mlb_spec.rb
+++ spec/mlb_spec.rb
@@ -3,59 +3,51 @@ require 'helper'
 describe MLB::Team, ".all" do
   context "with connection" do
     before do
-      stub_request(:get, 'http://api.freebase.com/api/service/mqlread').
-        with(:query => {:query => MLB::Team.mql_query}).
-        to_return(:body => fixture("teams.json"))
+      stub_request(:get, 'http://api.freebase.com/api/service/mqlread').with(:query => {:query => MLB::Team.mql_query}).to_return(:body => fixture("teams.json"))
     end
 
     after do
       MLB::Team.reset
     end
 
-    it "should request the correct resource" do
+    it "requests the correct resource" do
       MLB::Team.all
-      a_request(:get, 'http://api.freebase.com/api/service/mqlread').
-        with(:query => {:query => MLB::Team.mql_query}).
-        should have_been_made
+      expect(a_request(:get, 'http://api.freebase.com/api/service/mqlread').with(:query => {:query => MLB::Team.mql_query})).to have_been_made
     end
 
-    it "should return the correct results" do
+    it "returns the correct results" do
       teams = MLB::Team.all
-      teams.first.name.should == "Arizona Diamondbacks"
+      expect(teams.first.name).to eq "Arizona Diamondbacks"
     end
   end
 
   context "with timeout" do
     before do
-      stub_request(:get, 'http://api.freebase.com/api/service/mqlread').
-        with(:query => {:query => MLB::Team.mql_query}).
-        to_timeout
+      stub_request(:get, 'http://api.freebase.com/api/service/mqlread').with(:query => {:query => MLB::Team.mql_query}).to_timeout
     end
 
     after do
       MLB::Team.reset
     end
 
-    it "should return the correct results" do
+    it "returns the correct results" do
       teams = MLB::Team.all
-      teams.first.name.should == "Arizona Diamondbacks"
+      expect(teams.first.name).to eq "Arizona Diamondbacks"
     end
   end
 
   context "without connection" do
     before do
-      stub_request(:get, 'http://api.freebase.com/api/service/mqlread').
-        with(:query => {:query => MLB::Team.mql_query}).
-        to_raise(SocketError)
+      stub_request(:get, 'http://api.freebase.com/api/service/mqlread').with(:query => {:query => MLB::Team.mql_query}).to_raise(SocketError)
     end
 
     after do
       MLB::Team.reset
     end
 
-    it "should return the correct results" do
+    it "returns the correct results" do
       teams = MLB::Team.all
-      teams.first.name.should == "Arizona Diamondbacks"
+      expect(teams.first.name).to eq "Arizona Diamondbacks"
     end
   end
 end

c6e7f50a0aaae9cd5e46c63cc0d6f4b2e97eb2f9
Erik Michaels-Ober <sferik@gmail.com>
2013-02-09 22:53:16 -0800

    Remove faraday_middleware dependency

--- lib/mlb/request.rb
+++ lib/mlb/request.rb
@@ -1,5 +1,5 @@
 require 'faraday'
-require 'faraday_middleware'
+require 'mlb/response/parse_json'
 
 module MLB
   # @private
@@ -15,9 +15,9 @@ module MLB
     private
 
     def self.connection(raw=false)
-      Faraday.new(:url => 'http://api.freebase.com') do |connection|
-        connection.use FaradayMiddleware::ParseJson unless raw
-        connection.adapter Faraday.default_adapter
+      Faraday.new(:url => 'http://api.freebase.com') do |builder|
+        builder.use MLB::Response::ParseJson unless raw
+        builder.adapter Faraday.default_adapter
       end
     end
   end
